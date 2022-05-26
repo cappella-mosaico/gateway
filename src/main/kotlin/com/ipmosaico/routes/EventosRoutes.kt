8b8c392d719b1eea584c34cf3f3492188e98fa55
ruither.borba <delki8@gmail.com>
2022-05-26 18:55:40 -0300

    adding a call to the eventos service and removing the mock call

--- src/main/kotlin/com/ipmosaico/routes/EventosRoutes.kt
+++ src/main/kotlin/com/ipmosaico/routes/EventosRoutes.kt
@@ -1,10 +1,10 @@
-
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import khttp.get as httpGet
 
 @Serializable
 data class Evento(
@@ -20,21 +20,13 @@ data class Evento(
 
 fun Route.eventosRouting() {
 
+    val ROOT = "http://eventos:8080"
+
     route("/eventos") {
         get {
-            call.respond(listOf(
-                Json.decodeFromString<Evento>("""
-                  {
-                    "titulo" : "acampamento",
-                    "dataInicial" : "new Date(2021, 12, 8, 20, 0)",
-                    "dataFim" : "new Date(2021, 12, 8)",
-                    "imagemURL" : "https://reactjs.org/logo-og.png",
-                    "sobre" : "A Igreja Presbiteriana Mosaico existe para acolher pessoas e formar discípulos de Cristo através de relacionamentos saudáveis e uma pregação bíblica contemporânea no bairro Setor Bueno, na cidade de Goiânia e no mundo",
-                    "valor" : "R${'$'} 15,00 adulto R${'$'} 10,00 até 12 anos",
-                    "local" : "Na Igreja Presbiteriana Mosaico",
-                    "endereco" : "Rua T-53, 480, Setor Bueno Goiânia/Go - Cep 74810-210"
-                  }
-            """.trimIndent())))
+            val amount = call.request.queryParameters["amount"]
+            val response = httpGet("$ROOT/eventos?amount=$amount")
+            call.respondBytes { response.content }
         }
     }
 
