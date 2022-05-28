3163f294079273e7cf58f5e9655753c8131603bb
ruither.borba <delki8@gmail.com>
2022-05-28 18:02:49 -0300

    adding the post endpoint to eventos

--- src/main/kotlin/com/ipmosaico/routes/EventosRoutes.kt
+++ src/main/kotlin/com/ipmosaico/routes/EventosRoutes.kt
@@ -4,30 +4,44 @@ import io.ktor.server.routing.*
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import io.ktor.http.*
+import io.ktor.server.auth.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.json.JsonObject
 import khttp.get as httpGet
+import khttp.post as httpPost
 
-@Serializable
-data class Evento(
-    val titulo: String,
-    val dataInicial: String,
-    val dataFim: String,
-    val imagemURL: String,
-    val sobre: String,
-    val valor: String,
-    val local: String,
-    val endereco: String
-)
 
 fun Route.eventosRouting() {
 
     val ROOT = "http://eventos:8080"
 
     route("/eventos") {
-        get {
-            val amount = call.request.queryParameters["amount"]
-            val response = httpGet("$ROOT/eventos?amount=$amount")
-            call.respondBytes { response.content }
-        }
+	    get {
+		val amount = call.request.queryParameters["amount"]
+		val response = httpGet("$ROOT/eventos?amount=$amount")
+		call.respondBytes { response.content }
+	    }
+
+	authenticate ("auth-jwt") {
+	    post {
+		val evento = call.receive<JsonObject>()
+		println(evento)
+
+		val headers : Map<String, String> = call.request.headers.entries()
+		    .associate { Pair(it.key, it.value.get(0)) }.toMutableMap()
+		println(headers)
+		val response = httpPost(
+		    headers = headers,
+		    url = "$ROOT/eventos",
+		    data = evento.toString()
+		)
+		call.respondBytes { response.content }
+	    }
+	}
+	
     }
 
 }
