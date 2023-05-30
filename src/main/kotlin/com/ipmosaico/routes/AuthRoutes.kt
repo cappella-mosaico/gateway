16cd626ea9aa4621c258e0fbded6f25879a6701b
ruither.borba <delki8@gmail.com>
2023-05-30 10:21:06 -0300

    reading users from filesystem

--- src/main/kotlin/com/ipmosaico/routes/AuthRoutes.kt
+++ src/main/kotlin/com/ipmosaico/routes/AuthRoutes.kt
@@ -9,14 +9,27 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import java.util.*
+import java.security.MessageDigest
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.Json
+import java.io.File
+import java.io.InputStream
 
 fun Route.authRoutes() {
 
+    val inputStream: InputStream = File("/ipmosaico-users/users.json").inputStream()
+    val input = inputStream.bufferedReader().use { it.readText() }
+    val jsonObjo = Json.parseToJsonElement(input)
+    val users = jsonObjo.jsonObject.toMap()
+
+
     route("/login") {
         post {
+
             val user = call.receive<User>()
-            if (user.username == "ipmosaico"
-                && user.password == "mosaicoadmin2022") {
+            val passwordDigest = calculateSHA256Hash(user.password)
+            val storedPassword = users[user.username].toString().replace("\"", "")
+            if (storedPassword == passwordDigest) {
 
                 val secret = this@route.environment!!.config.property("jwt.secret").getString()
                 val issuer = this@route.environment!!.config.property("jwt.issuer").getString()
@@ -36,4 +49,20 @@ fun Route.authRoutes() {
         }
     }
 
+}
+
+fun calculateSHA256Hash(input: String): String {
+    val messageDigest = MessageDigest.getInstance("SHA-256")
+    val hashBytes = messageDigest.digest(input.toByteArray())
+
+    val hexString = StringBuilder()
+    for (byte in hashBytes) {
+        val hex = Integer.toHexString(0xFF and byte.toInt())
+        if (hex.length == 1) {
+            hexString.append('0')
+        }
+        hexString.append(hex)
+    }
+
+    return hexString.toString()
 }
--- src/test/kotlin/com/ipmosaico/ApplicationTest.kt
+++ src/test/kotlin/com/ipmosaico/ApplicationTest.kt
@@ -18,16 +18,42 @@ import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.ipmosaico.plugins.*
+import java.io.File
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.Test
 
 class ApplicationTest {
+
+    companion object {
+        private lateinit var tempFile: File
+
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            // Create a temporary file
+            tempFile = File("/ipmosaico-users/users.json")
+            tempFile.createNewFile()
+
+            // Write some content to the file
+            tempFile.writeText("""{
+                                   "ipmosaico": "sha256pass"
+                               }""")
+        }
+
+        @AfterClass
+        @JvmStatic
+        fun cleanup() {
+            // Delete the temporary file
+            tempFile.delete()
+        }
+    }
+
     @Test
     fun testRoot() = testApplication {
         application {
             configureRouting()
         }
-/*        client.post("/login").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
-        }*/
     }
+
 }
